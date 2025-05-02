e4e56452f314f124f6df96954fcce06bad845b5e
Javier Matias-Cabrera <javier@javiermatias.com>
2025-05-02 03:16:11 -0400

    Add code with bad formatting

--- src/merge_blocker_strawman/hola_mundo.py
+++ src/merge_blocker_strawman/hola_mundo.py
@@ -2,5 +2,12 @@ def main():
     print("Hola Mundo!")
 
 
+def bad_formatting():
+    # This function has bad formatting
+    x=1
+    y=2
+    z=x+y
+    print(z)
+
 if __name__ == "__main__":
     main()
